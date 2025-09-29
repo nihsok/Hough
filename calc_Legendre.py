8216e1b869f23397f873b380babcc31411412937
nihsok <8760998+nihsok@users.noreply.github.com>
2025-09-28 20:24:14 -0600

    hough calculation and plot completed, but small oscillation.

---
+++ calc_Legendre.py
@@ -0,0 +1,13 @@
+#calculate normalized associated Legendre polynomials
+from scipy.special import assoc_legendre_p_all
+import numpy as np
+
+m_max = 90 #order: m=m=s
+l_max= 2*m_max #degree: l=n=r
+
+lp = assoc_legendre_p_all(l_max,m_max,np.linspace(0,1,91),norm=True)
+#print(lp[0,:,:,:].shape) #lp(0:0,0:n,-m:m,0:90)
+with open("docs/nalegendre.bin",'wb') as f:
+  for m in range(m_max):
+    for l in range(m,m+m_max):
+      f.write(lp[0,l,m,:])
--- docs/calc_eigen.js
+++ docs/calc_eigen.js
@@ -30,9 +30,9 @@ function calc_eigen(){
   const row = document.getElementsByName('F1_row')
   for (let i=0;i<4;i++){
     let j = i==0 ? 2+i : i //rowspan for the first row
-    row[i].cells[j].innerText = M[i*2].toFixed(4)
-    row[i].cells[j+1].innerText = L[i*2].toFixed(4)
-    if(i<3) row[i+1].cells[i].innerText = L[i*2].toFixed(4)
+    row[i].cells[j].innerText = M[i*2].toFixed(3)
+    row[i].cells[j+1].innerText = L[i*2].toFixed(3)
+    if(i<3) row[i+1].cells[i].innerText = L[i*2].toFixed(3)
   }
 
   const len_sym = s%2==0 ? (n-s+2)/2 : (n-s+1)/2
@@ -49,21 +49,116 @@ function calc_eigen(){
   const lambda = ans.values._data
   const vector = ans.eigenvectors
   const x1 = document.getElementsByName('x1')
-  for (i=1;i<10;i++){
-    x1[0].cells[i+3].innerText =(lambda[len_sym-i]*ev2ed).toFixed(1)
-    x1[1].cells[i+3].innerText = lambda[len_sym-i].toFixed(4)
+  let a_sym = []
+  for (let i=1;i<=10;i++){
+    x1[0].cells[i+2].innerText =(lambda[len_sym-i]*ev2ed).toFixed(1)
+    x1[1].cells[i+2].innerText = lambda[len_sym-i].toFixed(4)
     if (lambda[len_sym-i]<0){
-      x1[0].cells[i+3].innerHTML = '<i>'+x1[0].cells[i+3].innerText+'</i>'
-      x1[1].cells[i+3].innerHTML = '<i>'+x1[1].cells[i+3].innerText+'</i>'
+      x1[0].cells[i+2].innerHTML = '<i>'+x1[0].cells[i+2].innerText+'</i>'
+      x1[1].cells[i+2].innerHTML = '<i>'+x1[1].cells[i+2].innerText+'</i>'
     }
-    x1[2].cells[i+3].innerText = vector[len_sym-i].vector._data[0].toFixed(4)
-    x1[3].cells[i+1].innerText = vector[len_sym-i].vector._data[1].toFixed(4)
-    x1[4].cells[i+1].innerText = vector[len_sym-i].vector._data[2].toFixed(4)
-    x1[5].cells[i+1].innerText = vector[len_sym-i].vector._data[3].toFixed(4)
-    x1[6].cells[i+1].innerText = '⋮'
+    x1[2].cells[i+2].innerText = vector[len_sym-i].vector._data[0].toFixed(3)
+    x1[3].cells[i].innerText = vector[len_sym-i].vector._data[1].toFixed(3)
+    x1[4].cells[i].innerText = vector[len_sym-i].vector._data[2].toFixed(3)
+    x1[5].cells[i].innerText = vector[len_sym-i].vector._data[3].toFixed(3)
+    x1[6].cells[i].innerText = '⋮'
+
+    a_sym[i-1] = vector[len_sym-i].vector._data
   }
+  const table = document.getElementById('X1')
+  let currentindex = null
+  table.addEventListener("mouseover",function(e){
+    const cell = e.target.closest("td")
+    if(!cell) return
+    const colIndex = cell.cellIndex
+    const rowIndex = cell.parentNode.rowIndex
+    if(colIndex-(rowIndex>2 ? 1 : 3) === currentindex) return
+    if(rowIndex<3 && colIndex<3 || rowIndex>2 && colIndex<1) return
+    if(currentindex !== null){
+      Array.from(table.rows).forEach((row,i)=>{
+        row.cells[currentindex+(i>2 ? 1 : 3)].classList.remove("highlight")
+      })
+    }
+    currentindex = colIndex-(rowIndex>2 ? 1 : 3)
+    Array.from(table.rows).forEach((row,i)=>{
+      row.cells[colIndex-(i>2 ? 2 : 0)+(rowIndex>2 ? 2 : 0)].classList.add("highlight")
+    })
+    plot_hough(s,0,a_sym[currentindex])
+  })
   //antisymmetric mode
 }
 
-//highlight column on hover
-//plot
+let p_r_s
+async function loadLegendre(){
+  const response = await fetch('nalegendre.bin')
+  const buffer = await response.arrayBuffer()
+  p_r_s = new Float64Array(buffer)
+}
+
+const canvas = document.getElementById("canvas")
+const margin = {bottom:10,top:10,left:20,right:10}
+const plot_height = canvas.height-margin.left-margin.top
+const plot_width = canvas.width-margin.left-margin.right
+const ctx = canvas.getContext('2d')
+ctx.translate(plot_width/2+margin.left,plot_height/2+margin.top)
+ctx.font = '20px sans-serif'
+ctx.textAlign = 'center'
+ctx.textBaseline = 'middle'
+ctx.save()
+const scaleX = (x) => x*plot_width/180
+const scaleY = (y) => -y*plot_height/6
+
+function plot_hough(s,flag_asymmetric,coef){
+  //add title
+
+  const nlat = 91
+  const m_max = 90
+  let hough = new Float64Array(nlat)
+  for (let i=flag_asymmetric;i<n/2;i++){
+    const start = s*m_max*m_max+2*i*nlat
+    const slice = p_r_s.subarray(start,start+nlat)
+    for (let j=0;j<nlat;j++){
+      hough[j]+= coef[i]*slice[j]
+    }
+  }
+  const hough_t = Array.from(hough.subarray(1)).reverse().concat(Array.from(hough))
+  const hough_u = []
+  const hough_v = []
+  console.log(hough_t)
+
+  ctx.clearRect(-plot_width/2-margin.left,-plot_height/2-margin.top,canvas.width,canvas.height)
+  ctx.restore()
+  ctx.lineWidth = 2
+  ctx.strokeRect(scaleX(-90),scaleY(-3),scaleX(180),scaleY(6))
+  ctx.lineWidth = 0.5
+  ctx.setLineDash([3,3])
+  for (let i=-60;i<90;i+=30){
+    ctx.beginPath()
+    ctx.moveTo(scaleX(i),scaleY(-3))
+    ctx.lineTo(scaleX(i),scaleY(3))
+    ctx.stroke()
+    ctx.fillText(String(i)+'°',scaleX(i),scaleY(-3.3))
+  }
+  for (let i=-2;i<3;i+=1){
+    ctx.beginPath()
+    ctx.moveTo(scaleX(-90),scaleY(i))
+    ctx.lineTo(scaleX(90),scaleY(i))
+    ctx.stroke()
+    ctx.fillText(String(i),scaleX(-94),scaleY(i))
+  }
+
+  ctx.restore()
+  ctx.lineWidth = 3
+  ctx.setLineDash([])
+  ctx.beginPath()
+  ctx.moveTo(scaleX(-90),scaleY(hough_t[0]))
+  for (let i=1;i<hough_t.length;i++){
+    ctx.lineTo(scaleX(i-90),scaleY(hough_t[i]))
+  }
+  ctx.stroke()
+
+}
+window.addEventListener('load',async () => {
+  await loadLegendre();
+})
+
